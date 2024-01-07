89ecdf41ba1c0815f56a58794b400cd77489c8d6
harpreet1o <harpreetx01@outlook.com>
2024-01-06 22:20:09 -0800

    newgoal

--- frontend/vite-project/src/Pages/ShopCategory.jsx
+++ frontend/vite-project/src/Pages/ShopCategory.jsx
@@ -7,16 +7,16 @@ export const ShopCategory = (props) => {
 
   return (
     <div>
-      <img src={props.banner} alt="" className="" />
-      <div>
+      <img src={props.banner} alt="" className="block my-8 mx-auto w-5/6 " />
+      <div className="flex mx-40 justify-between items-center">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span className="text-semibold">Showing 1-12</span> out of 36 products
         </p>
-        <div>
+        <div className="px-4 py-2 rounded-3xl border-solid border-2 border-slate-600 flex items-center gap-1">
           Sort by <img src={dropdownIcon} alt="" />
         </div>
       </div>
-      <div>
+      <div className="mx-4 my-5 grid grid-cols-4 gap-x-20">
         {allProduct.map((item, i) => {
           if (props.category === item.category) {
             return (
@@ -31,6 +31,9 @@ export const ShopCategory = (props) => {
             );
           } else return;
         })}
+      </div>
+      <div className="flex justify-center items-center my-36 mx-auto w-60 h-16 rounded-3xl bg-slate-100 text-gray-500 text-medium ">
+        Explore More
       </div>
     </div>
   );
