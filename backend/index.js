e1beb6d25eebaeb0ef25109e4a14994ea1b096c6
harpreet1o <harpreetx01@outlook.com>
2024-01-09 00:50:57 -0800

    lets go

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,44 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use;
-app.listen;
-const port = 3000;
+
+// Database coonection
+mongoose.connect(
+  "mongodb+srv://harpreetsinghthechamp:Hecommerce@cluster0.palm6ol.mongodb.net"
+);
+
+//api creation
+app.get("/", (req, res) => {
+  res.send("express app is running");
+});
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+
+app.use("/images", express.static("upload/images"));
+
+//Creating upload Endpoint for images
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+app.listen(port, (error) => {
+  if (!error) console.log(`Server running on ${port}`);
+  else console.log(`${error} error`);
+});
